8970968015b46a1110a83713a1054813f00800dc
DjadjevHD <123975457+AlehandroDjadjev@users.noreply.github.com>
2023-12-06 17:15:50 +0200

    Savable Entity completion
    - Added unique identifier.
    - Can now capture and restore states.

--- Assets/GameComponents/Scripts/Saving/SavaebleEntity.cs
+++ Assets/GameComponents/Scripts/Saving/SavaebleEntity.cs
@@ -3,37 +3,54 @@ using System.Collections.Generic;
 using System.Text;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.AI;
+using RPG.Core;
 
 
-[ExecuteAlways]
-public class SavaebleEntity : MonoBehaviour
+namespace RPG.Saving
 {
-    [SerializeField] string uniqueIdentifier = "";
-    public string GetUniqueIdentifier()
+    [ExecuteAlways]
+    public class SavaebleEntity : MonoBehaviour
     {
-        return uniqueIdentifier;
-    }
-
-    public object CaptureState()
-    {
-        return null;
-    }
-
-    public void RestoreState(object state)
-    {
-        print("Hallo");
-    }
-
-    private void Update()
-    {
-        if (Application.IsPlaying(gameObject)) return;
-        if (gameObject.scene.path == "") return;
-        SerializedObject serializedObject = new SerializedObject(this);
-        SerializedProperty property = serializedObject.FindProperty("uniqueIdentifier");
-        if(property.stringValue == "")
+        [SerializeField] string uniqueIdentifier = "";
+        public string GetUniqueIdentifier()
         {
-            property.stringValue = System.Guid.NewGuid().ToString();
-            serializedObject.ApplyModifiedProperties();
+            return uniqueIdentifier;
+        }
+
+        public object CaptureState()
+        {
+            return new SavaebleVector3(transform.position);
+        }
+
+        public void RestoreState(object state)
+        {
+            RestorePlayerPostion(state);
+        }
+
+        private void RestorePlayerPostion(object state)
+        {
+            GetComponent<ActionSchedular>().CancelCurrentAction();
+            GetComponent<NavMeshAgent>().enabled = false;
+            SavaebleVector3 savedPosition = (SavaebleVector3)state;
+            transform.position = savedPosition.ToVector3();
+            GetComponent<NavMeshAgent>().enabled = true;
+        }
+
+
+        //#if UNITY_EDITOR
+        private void Update()
+        {
+            if (Application.IsPlaying(gameObject)) return;
+            if (string.IsNullOrEmpty(gameObject.scene.path)) return;
+            SerializedObject serializedObject = new SerializedObject(this);
+            SerializedProperty property = serializedObject.FindProperty("uniqueIdentifier");
+            if (string.IsNullOrEmpty(property.stringValue))
+            {
+                property.stringValue = System.Guid.NewGuid().ToString();
+                serializedObject.ApplyModifiedProperties();
+            }
         }
     }
 }
+   // #endif
